fa884a7871ffcc524bb932b8df53328ef6120376
Marcos Ferreira <marcosradix@gmail.com>
2018-11-18 10:30:59 -0300

    gerando refresh token

--- src/main/java/br/com/workmade/cursomc/resources/CategoriaResources.java
+++ src/main/java/br/com/workmade/cursomc/resources/CategoriaResources.java
@@ -10,7 +10,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,7 +24,6 @@ import br.com.workmade.cursomc.service.CategoriaService;
 
 @RestController
 @RequestMapping(value={"/"})
-@CrossOrigin(origins = "*")
 public class CategoriaResources {
 	
 	@Autowired
--- src/main/java/br/com/workmade/cursomc/resources/PedidoResources.java
+++ src/main/java/br/com/workmade/cursomc/resources/PedidoResources.java
@@ -15,9 +15,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import br.com.workmade.cursomc.domain.Categoria;
 import br.com.workmade.cursomc.domain.Pedido;
-import br.com.workmade.cursomc.dto.CategoriaDTO;
 import br.com.workmade.cursomc.service.PedidoService;
 
 @RestController
