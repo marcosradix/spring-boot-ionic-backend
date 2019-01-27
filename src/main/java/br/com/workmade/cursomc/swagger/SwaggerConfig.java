7f494fc7d312ff8de2e781b46faaf11b1ceb2003
Marcos Ferreira <marcosradix@gmail.com>
2019-01-27 14:48:36 -0300

    controlando exibição do swagger via application.properties profile

--- src/main/java/br/com/workmade/cursomc/swagger/SwaggerConfig.java
+++ src/main/java/br/com/workmade/cursomc/swagger/SwaggerConfig.java
@@ -2,6 +2,7 @@ package br.com.workmade.cursomc.swagger;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
@@ -14,6 +15,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
+@Profile(value= {"dev","test"})
 public class SwaggerConfig {
 
 	
